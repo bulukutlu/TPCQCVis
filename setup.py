044667e896e08af51bbd2afb835e7d67bb38fce8
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2022-05-23 22:57:09 +0200

    made pip package

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author_email='berkin.ulukutlu@cern.ch',
+    author='Berkin Ulukutlu',
+    url='https://github.com/bulukutlu/TPCQCVis',
+    name='TPCQCVis',
+    version='v0.1', 
+    #packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["scripts*", "tests*","*d.ts"]),
+    license='Not defined yet. Most probably similar to ALICE (CERN)  license',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=False,
+    package_data={
+    '': ['../*/*/*/*.ts']
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        ##---------------------   graphics  dependencies
+        'bokeh',
+        # ----------------------   jupyter notebook dependencies
+        'ipywidgets',
+        'runtime',
+        'requests',
+        'rise'
+    ]
+)
