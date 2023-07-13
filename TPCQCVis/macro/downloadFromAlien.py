0256ff742420db2dd45f09dcec758bee090ecf05
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2023-07-13 09:26:52 +0200

    Downloading made easier

--- TPCQCVis/macro/downloadFromAlien.py
+++ TPCQCVis/macro/downloadFromAlien.py
@@ -1,23 +1,53 @@
 import os
 import subprocess
+import sys
+import time
 
-local_dir = "/home/berki/alice/data/2022/LHC22s/"
-directory = "/alice/data/2022/LHC22s/"
-runList = ["529397","529399","529403","529414","529418"]
-apass = "apass4"
+def getRunList(remote_dir):
+    directories = subprocess.run(["alien_ls", remote_dir], capture_output=True)
+    # Remove unnecessary characters and split the string into individual elements
+    cleaned_string = directories.stdout.decode("utf-8").replace("b'", "").replace("\n", "").replace("'", "")
+    elements = cleaned_string.split("/")
+    runList = [element for element in elements if (element and (int(element)>100000))]
+    print("Found",len(runList),"runs in", remote_dir)
+    return runList
 
-for run in runList:
-    target = subprocess.run(["alien.py","find",directory+run+"/"+apass+"/QC/001/","QC.root"], capture_output=True)
-    if len(target.stdout) > 0 : 
-        target_path = target.stdout[:-1].decode('UTF-8')
-        print("Downloading "+target_path)
-        subprocess.run(["alien.py","cp","alien:"+target_path,"file:"+local_dir+run+".root"])
-    else:
-        target = subprocess.run(["alien.py","find",directory+run+"/"+apass+"/","QC_fullrun.root"], capture_output=True)
-        if len(target.stdout) > 0 : 
+def downloadFiles(local_dir, remote_dir, production, runList):
+    for run in runList:
+        print("Searching run",run)
+        target = subprocess.run(["alien.py", "find", remote_dir + run + "/" + production + "/QC/001/", "QC.root"], capture_output=True)
+        if len(target.stdout) > 0:
             target_path = target.stdout[:-1].decode('UTF-8')
-            print("Downloading "+target_path)
-            subprocess.run(["alien.py","cp","alien:"+target_path,"file:"+local_dir+run+".root"])
-        else: 
-            print("File "+directory+run+"/"+apass+"/QC/001/QC.root"+ "  not found!")
+            print("Downloading " + target_path)
+            subprocess.run(["alien.py", "cp", "alien:" + target_path, "file:" + local_dir + run + ".root"])
+        else:
+            target = subprocess.run(["alien.py", "find", remote_dir + run + "/" + production + "/", "QC_fullrun.root"], capture_output=True)
+            if len(target.stdout) > 0:
+                target_path = target.stdout[:-1].decode('UTF-8')
+                print("Downloading " + target_path)
+                subprocess.run(["alien.py", "cp", "alien:" + target_path, "file:" + local_dir + run + ".root"])
+            else:
+                print("File " + remote_dir + run + "/" + production + "/QC/001/QC.root" + " not found!")
+        time.sleep(1) #otherwise too many requests
 
+# Get the command-line arguments
+args = sys.argv[1:]
+
+# Check if the correct number of arguments is provided
+if len(args) < 3:
+    print("Insufficient number of arguments provided.")
+    print("Usage: python downloadFromAlien.py local_dir remote_dir production [runList]")
+    sys.exit(1)
+
+# Assign the command-line arguments to variables
+local_dir = args[0]
+remote_dir = args[1]
+production = args[2]
+runList = args[3:] if len(args) > 3 else None
+
+# If runList is not provided, obtain it dynamically
+if runList is None:
+    runList = getRunList(remote_dir)
+
+# Download files
+downloadFiles(local_dir, remote_dir, production, runList)
