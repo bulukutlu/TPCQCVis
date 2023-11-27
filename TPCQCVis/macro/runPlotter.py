a8bebbf65cb81abbaccd6fb0db94d7f76cd03373
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2023-11-27 18:46:11 +0100

    make plotter able to take direct path

--- TPCQCVis/macro/runPlotter.py
+++ TPCQCVis/macro/runPlotter.py
@@ -8,48 +8,53 @@ import argparse
 ROOT.gROOT.LoadMacro("/home/berki/Software/TPCQCVis/TPCQCVis/macro/plotQCData.C+")
 ROOT.gROOT.SetBatch(True)
 
-def main(local_dir, add_run_param, rerun):
-    os.system("cd " + local_dir)
+def main(local_dir, add_run_param, rerun, target):
+    if target:
+        if os.path.isfile(target):
+                print("Plotting target", target)
+                test = ROOT.plotQCData(target)
     
-    fileList_processed = glob.glob(local_dir + "*_QC.root")
-    runList_processed = [file[-14:-8] for file in fileList_processed]
+    else:
+        os.system("cd " + local_dir)
+    
+        fileList_processed = glob.glob(local_dir + "*_QC.root")
+        runList_processed = [file[-14:-8] for file in fileList_processed]
 
-    fileList = glob.glob(local_dir + "*.root")
-    fileList = [file for file in fileList if file not in fileList_processed]
-    runList = [file[file.rfind('/')+1:-5] for file in fileList]
+        fileList = glob.glob(local_dir + "*.root")
+        fileList = [file for file in fileList if file not in fileList_processed]
+        runList = [file[file.rfind('/')+1:-5] for file in fileList]
 
-    if not rerun:
-        runList = [run for run in runList if run not in runList_processed]
-
-    for run in runList:
-        path = local_dir + run + ".root"
-        print("Path:", path)
-        if os.path.isfile(path):
-            print("Plotting run", run)
-            test = ROOT.plotQCData(path)
-        if add_run_param:
-            command = 'o2-calibration-get-run-parameters -r ' + run
-            print("Adding parameters for run", command)
-            os.system(command)
-            with open('IR.txt') as f:
-                lines = f.readlines()
-            IR = array('d', [float(lines[0])])
-            with open('Duration.txt') as f:
-                lines = f.readlines()
-            Duration = array('d', [float(lines[0])])
-            with open('BField.txt') as f:
-                lines = f.readlines()
-            BField = array('d', [float(lines[0])])
-            output = ROOT.TFile(local_dir + run + "_QC.root", "update")
-            tree = ROOT.TTree("RunParameters", "RunParameters")
-            tree.Branch("IR", IR, 'IR/D')
-            tree.Branch("Duration", Duration, 'Duration/D')
-            tree.Branch("BField", BField, 'BField/D')
-            tree.Fill()
-            output.Write()
-            os.remove("IR.txt")
-            os.remove("Duration.txt")
-            os.remove("BField.txt")
+        if not rerun:
+            runList = [run for run in runList if run not in runList_processed]
+        for run in runList:
+            path = local_dir + run + ".root"
+            print("Path:", path)
+            if os.path.isfile(path):
+                print("Plotting run", run)
+                test = ROOT.plotQCData(path)
+            if add_run_param:
+                command = 'o2-calibration-get-run-parameters -r ' + run
+                print("Adding parameters for run", command)
+                os.system(command)
+                with open('IR.txt') as f:
+                    lines = f.readlines()
+                IR = array('d', [float(lines[0])])
+                with open('Duration.txt') as f:
+                    lines = f.readlines()
+                Duration = array('d', [float(lines[0])])
+                with open('BField.txt') as f:
+                    lines = f.readlines()
+                BField = array('d', [float(lines[0])])
+                output = ROOT.TFile(local_dir + run + "_QC.root", "update")
+                tree = ROOT.TTree("RunParameters", "RunParameters")
+                tree.Branch("IR", IR, 'IR/D')
+                tree.Branch("Duration", Duration, 'Duration/D')
+                tree.Branch("BField", BField, 'BField/D')
+                tree.Fill()
+                output.Write()
+                os.remove("IR.txt")
+                os.remove("Duration.txt")
+                os.remove("BField.txt")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script for processing data")
@@ -64,6 +69,7 @@ if __name__ == "__main__":
         action="store_true",
         help="Rerun plotter for existing files (default: False)"
     )
+    parser.add_argument("--target", help="Directly plot target ROOT file")
     args = parser.parse_args()
 
-    main(args.local_dir, args.add_run_param, args.rerun)
+    main(args.local_dir, args.add_run_param, args.rerun, args.target)
