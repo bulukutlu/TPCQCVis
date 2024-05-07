2f603f1d8b49c188632c78b747c9cfa833bf96d7
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2024-05-07 17:37:00 +0200

    Added option for downloadSim to download for all runs in path

--- TPCQCVis/tools/downloadSim.py
+++ TPCQCVis/tools/downloadSim.py
@@ -24,15 +24,22 @@ import time
 
 LOCALDIR = "/cave/alice-tpc-qc/data/sim/"
 
+def getPaths(path):
+    runs = subprocess.run(["alien_ls", path], capture_output=True)
+    runs = runs.stdout.splitlines()
+    print("Found", len(runs), "runs")
+    return [path+run.decode('UTF-8')+"QC" for run in runs]
+
 def downloadFromAlien(new_productions):
     # Downloading from alien
     downloadedFiles = []
-    for path in new_productions:
+    for i,path in enumerate(new_productions):
         year = path.split("/")[3]
         period = path.split("/")[4]
         runNumber = path.split("/")[6]
         target_path = path + "/tpcStandardQC.root"
         local_path  = LOCALDIR+"/"+year+"/"+period+"/"+runNumber+".root"
+        if not i: print("Downloading to", local_path)
         print("Downloading " + target_path)
         #print("Executing:",["alien.py", "cp", "alien:" + target_path, "file:"+local_path])
         subprocess.run(["alien.py", "cp", "alien:" + target_path, "file:"+local_path])
@@ -46,6 +53,12 @@ def downloadFromAlien(new_productions):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script for reading list of alien paths containing simulation QC, and downloading.")
     parser.add_argument("--paths",nargs="*",type=str, help="Paths of merged QC output")
+    parser.add_argument("--dir",type=str, help="Path to dir of all runs (e.g. /alice/sim/2024/LHC24d2b/0/)")
     args = parser.parse_args()
 
-    downloadedFiles = downloadFromAlien(args.paths)
+    fileList = []
+    if args.dir:
+        fileList = getPaths(args.dir)
+    else:
+        fileList = args.paths
+    downloadedFiles = downloadFromAlien(fileList)
